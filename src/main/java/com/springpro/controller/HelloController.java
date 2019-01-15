5305436ab1c7b5f5c405d6c8775a88d0acce98c4
Bùi Quốc Khánh <khanhbq_58@vnu.edu.vn>
2019-01-15 17:14:26 +0700

    test

--- src/main/java/com/springpro/controller/HelloController.java
+++ src/main/java/com/springpro/controller/HelloController.java
@@ -1,15 +1,22 @@
 package com.springpro.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+		
+	@Autowired
+	JdbcTemplate jdbcTemplate;
 	
 	@GetMapping("/hello")
 	public String hello(Model model) {
-		model.addAttribute("user", "Koas = Choas");
+		String sql = "select 1 from dual";
+		int s = jdbcTemplate.queryForObject(sql,Integer.class);
+		model.addAttribute("user", s);
 		
 		return "hello";
 	}
